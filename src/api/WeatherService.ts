20a118efc209fd5b45fb9f6b001212bbcbfe6cf1
Vadim Medvedev <vadim130809@gmail.com>
2024-02-16 22:14:03 +0300

    change fixed location to changable

--- src/api/WeatherService.ts
+++ src/api/WeatherService.ts
@@ -1,6 +1,6 @@
 export default class WeatherService {
-  static async getData():Promise<void> {
-    return await fetch('https://api.openweathermap.org/data/2.5/forecast?lat=54.198705&lon=37.641062&appid=40dd0d9fca4d02c5fa33d8b285cdeb4f')
+  static async getData(lat: string, lon: string):Promise<void> {
+    return await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=40dd0d9fca4d02c5fa33d8b285cdeb4f`)
       .then((response) => response.json())
       .then((response) => response?.list)
       .catch((reject):void => console.log(reject))
