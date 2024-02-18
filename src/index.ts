4782ff0c36e102b243731e7c02cc50a72e60425c
Vadim Medvedev <vadim130809@gmail.com>
2024-02-18 13:26:06 +0300

    add working header with form

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
 import './index.css'
 
 import WeatherService from './api/WeatherService'
+
+const $buttonShowLocationForm = document
+  .querySelector('[data-button-show-location-form]')
+const $formLocation = document.querySelector('[data-form-location]')
+
+$buttonShowLocationForm!.addEventListener('click', ():void => {
+  const isFormHidden:boolean = $formLocation?.ariaHidden === 'true'
+  
+  if (isFormHidden) {
+    $formLocation!.ariaHidden = 'false'
+    ;($buttonShowLocationForm as HTMLInputElement)
+      .src = 'public/close.svg'
+    $formLocation?.classList.add('flex')
+    return $formLocation?.classList.remove('hidden')
+  }
+
+  $formLocation!.ariaHidden = 'true'
+  ;($buttonShowLocationForm as HTMLInputElement)
+    .src = 'public/search.svg'
+  $formLocation?.classList.add('hidden')
+  return $formLocation?.classList.remove('flex')
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'slide-top': {
+          'from': { top: '0', 'z-index': '-10' },
+          '50%': { top: '6rem' },
+          '100%': { 'z-index': '10', top: '6rem' }
+        }
+      },
+      animation: {
+        'slide-top': '400ms linear slide-top forwards'
+      }
+    }
   },
-  plugins: []
+  plugins: [],
 }
